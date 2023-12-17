6dd3417dce5e9a81105a08d4f10c12b63dacdaa8
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2023-12-17 21:38:09 +0300

    added ui

--- flourManager_java/flourManager_ui/pages/index.vue
+++ flourManager_java/flourManager_ui/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<b-container style="text-align: center; justify-content: center">
+<b-container style="background: olive; text-align: center; justify-content: center">
   <b-row>
     <b-col style="padding: 5%;">
     <ul class="tools">
@@ -9,6 +9,17 @@
       <li class="txt">=</li>
       <li class="flour"><b-badge variant="primary">0</b-badge></li>
     </ul>
+      <div>
+        <h3>Water: 0</h3>
+        <b-progress :value="3" :max ="10" animated variant="primary"></b-progress>
+        <h3>Energy: 0</h3>
+        <b-progress :value="4" :max ="10" animated variant="warning"></b-progress>
+        <h3>Millet: 0</h3>
+        <b-progress :value="4" :max ="10" animated variant="danger"></b-progress>
+      </div>
+      <div>
+        <b-img src = "/water_mill.gif"></b-img>
+      </div>
     </b-col>
   </b-row>
 </b-container>
@@ -26,7 +37,7 @@
   width: 80px; height: 80px; border-radius: 50%; border: #35495e 3px solid;
 }
 .tools li.txt{
-  border: none; line-height: 80px;
+  border: none; line-height: 80px; color: white; font-size: 300%;
 }
 .tools li.water{
   background: #fff url(/water.ico) no-repeat center center/cover;
