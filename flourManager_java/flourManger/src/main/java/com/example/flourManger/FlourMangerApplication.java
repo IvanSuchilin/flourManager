b1705f50fa11ae18c2c93ff92f4d732fd0d868ce
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2023-12-17 17:37:21 +0300

    added start icons

---
+++ flourManager_java/flourManger/src/main/java/com/example/flourManger/FlourMangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flourManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlourMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlourMangerApplication.class, args);
+	}
+
+}
